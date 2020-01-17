7a0268caeba071b842b9346d747ea52f07c1a193
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-01-17 14:11:58 +0000

    typoo

--- src/RestEaseClientGenerator.VSIX/Options/RestEase/RestEaseOptionsPage.cs
+++ src/RestEaseClientGenerator.VSIX/Options/RestEase/RestEaseOptionsPage.cs
@@ -56,7 +56,7 @@ namespace RestEaseClientGenerator.VSIX.Options.RestEase
 
         [Category(Interface)]
         [DisplayName("Method ReturnType")]
-        [Description("The ReturnType to use for the methods. The default value is 'Type'. For more details see https://github.com/canton7/RestEase#return-types.")]
+        [Description("The ReturnType to use for the methods. The default value is 'T'. For more details see https://github.com/canton7/RestEase#return-types.")]
         public MethodReturnType MethodReturnType { get; set; }
 
         [Category(Interface)]
--- src/RestEaseClientGenerator/Builders/InterfaceBuilder.cs
+++ src/RestEaseClientGenerator/Builders/InterfaceBuilder.cs
@@ -19,6 +19,7 @@ namespace RestEaseClientGenerator.Builders
             var builder = new StringBuilder();
             builder.AppendLine("using System;");
             builder.AppendLine("using System.Collections.Generic;");
+            builder.AppendLine("using System.IO;");
             builder.AppendLine("using System.Net.Http;");
             builder.AppendLine("using System.Net.Http.Headers;");
             builder.AppendLine("using System.Threading.Tasks;");
