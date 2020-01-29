f3f77e38ef0e0f15119d4e7469d561f5c8254219
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-01-29 19:25:11 +0100

    raml

---
+++ examples/RamlTest/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.OpenApi.Models;
+using SharpYaml.Serialization;
+
+namespace RamlTest
+{
+    public static class Program
+    {
+        public static string Get(this IDictionary<object, object> d, string key)
+        {
+            return d.ContainsKey(key) ? d[key] as string : null;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            var se = new SerializerSettings
+            {
+                //NamingConvention = new IgnoreCasetNamingConvention()
+            };
+
+            var serializer = new Serializer(se);
+
+            var o = serializer.Deserialize<IDictionary<object, object>>(File.OpenRead("MediaWiki.raml"));
+
+            int x = 0;
+
+            var doc = new OpenApiDocument
+            {
+                Info = MapInfo(o),
+                Paths = MapPaths(o)
+            };
+
+            int y = 9;
+        }
+
+        private static OpenApiPaths MapPaths(IDictionary<object, object> o)
+        {
+            var paths = new OpenApiPaths();
+
+            foreach (var key in o.Keys.OfType<string>().Where(k => k.StartsWith("/")))
+            {
+                paths.Add(key, MapPathItem(o[key] as IDictionary<object, object>));
+            }
+
+            return paths;
+        }
+
+        private static OpenApiPathItem MapPathItem(IDictionary<object, object> values)
+        {
+            return new OpenApiPathItem
+            {
+                Description = values.Get("description"),
+                Parameters = new List<OpenApiParameter>()
+            };
+        }
+
+        private static OpenApiInfo MapQ(IDictionary<object, object> o)
+        {
+            return null;
+        }
+
+        private static OpenApiInfo MapInfo(IDictionary<object, object> o)
+        {
+            return new OpenApiInfo
+            {
+                Title = o.Get("title"),
+                Description = o.Get("description"),
+                Version = o.Get("version")
+            };
+        }
+    }
+}
---
+++ src/OpenApiRamlConverter/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SharpYaml.Serialization;
+
+namespace OpenApiRamlConverter
+{
+    public class RamlConverter
+    {
+        public void X(Stream stream)
+        {
+            var serializer = new Serializer();
+
+            var se = new SerializerSettings();
+            
+
+            var oboject = serializer.Deserialize(stream);
+
+
+            var text = serializer.Serialize(new { List = new List<int>() { 1, 2, 3 }, Name = "Hello", Value = "World!" });
+            Console.WriteLine(text);
+        }
+    }
+}
