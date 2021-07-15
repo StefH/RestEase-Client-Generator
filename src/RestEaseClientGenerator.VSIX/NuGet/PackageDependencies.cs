c06ab418ef935d98bb52733159c25970ca9681db
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2021-07-15 08:37:35 +0200

    Upgrade RestEase to "1.5.5" (#26)

--- src/RestEaseClientGenerator.VSIX/NuGet/PackageDependencies.cs
+++ src/RestEaseClientGenerator.VSIX/NuGet/PackageDependencies.cs
@@ -4,9 +4,6 @@ namespace RestEaseClientGenerator.VSIX.NuGet
 {
     public static class PackageDependencies
     {
-        public static readonly PackageDependency RestEase =
-            new PackageDependency(
-                "RestEase",
-                new Version(1, 4, 10, 0));
+        public static readonly PackageDependency RestEase = new PackageDependency("RestEase", new Version(1, 5, 5, 0));
     }
 }
