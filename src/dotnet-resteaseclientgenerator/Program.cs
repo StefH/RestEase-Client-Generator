42a5fdd8cf4d51f4b294a9870ab93cd6f5419af1
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-12-20 16:39:57 +0000

    .

--- src/dotnet-resteaseclientgenerator/Program.cs
+++ src/dotnet-resteaseclientgenerator/Program.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using System.IO;
 using CommandLine;
 using DotNetRestEaseClientGenerator.Utils;
@@ -24,89 +23,89 @@ public class Program
         public bool SingleFile { get; set; }
 
         #region General
-        [Option(HelpText = "The Array type to use.", Default = ArrayType.Array)]
+        [Option("ArrayType", HelpText = "The Array type to use.", Default = ArrayType.Array)]
         public ArrayType ArrayType { get; set; }
 
-        [Option('n', HelpText = "The Namespace to use for the generated class file(s).", Required = true)]
+        [Option('n', "Namespace", HelpText = "The Namespace to use for the generated class file(s).", Required = true)]
         public string Namespace { get; set; }
 
-        [Option(HelpText = "Use DateTimeOffset instead of DateTime.", Default = false)]
+        [Option("UseDateTimeOffset", HelpText = "Use DateTimeOffset instead of DateTime.", Default = false)]
         public bool UseDateTimeOffset { get; set; }
 
-        [Option(HelpText = "Append this namespace for the Api.", Default = "Api")]
+        [Option("ApiNamespace", HelpText = "Append this namespace for the Api.", Default = "Api")]
         public string ApiNamespace { get; set; }
 
-        [Option(HelpText = "Append this namespace for the Models.", Default = "Models")]
+        [Option("ModelsNamespace", HelpText = "Append this namespace for the Models.", Default = "Models")]
         public string ModelsNamespace { get; set; }
 
-        [Option('g', HelpText = "Define what should be generated.", Default = GenerationType.Both)]
+        [Option('g', "GenerationType", HelpText = "Define what should be generated.", Default = GenerationType.Both)]
         public GenerationType GenerationType { get; set; }
 
-        [Option('e', HelpText = "Preferred Enum Type to generate. In case 'enum' is selected, enum classes are generated if needed.", Default = EnumType.String)]
+        [Option('e', "PreferredEnumType", HelpText = "Preferred Enum Type to generate. In case 'enum' is selected, enum classes are generated if needed.", Default = EnumType.String)]
         public EnumType PreferredEnumType { get; set; }
         #endregion
 
         #region Interface
-        [Option('a', HelpText = "The base FileName of the generated .cs files.", Required = true)]
+        [Option('a', "ApiName", HelpText = "The base FileName of the generated .cs files.", Required = true)]
         public string ApiName { get; set; }
 
-        [Option(HelpText = "The ReturnType to use for the methods. For more details see https://github.com/canton7/RestEase#return-types.", Default = MethodReturnType.Type)]
+        [Option("MethodReturnType", HelpText = "The ReturnType to use for the methods. For more details see https://github.com/canton7/RestEase#return-types.", Default = MethodReturnType.Type)]
         public MethodReturnType MethodReturnType { get; set; }
 
-        [Option(HelpText = "Append Async postfix to all methods.", Default = true)]
+        [Option("AppendAsync", HelpText = "Append Async postfix to all methods.", Default = true)]
         public bool AppendAsync { get; set; } = true;
 
-        [Option(HelpText = "The MultipartFormData FileType to use.", Default = MultipartFormDataFileType.ByteArray)]
+        [Option("MultipartFormDataFileType", HelpText = "The MultipartFormData FileType to use.", Default = MultipartFormDataFileType.ByteArray)]
         public MultipartFormDataFileType MultipartFormDataFileType { get; set; }
 
-        [Option(HelpText = "The application/octet-stream type to use.", Default = MultipartFormDataFileType.ByteArray)]
+        [Option("ApplicationOctetStreamType", HelpText = "The application/octet-stream type to use.", Default = MultipartFormDataFileType.ByteArray)]
         public ApplicationOctetStreamType ApplicationOctetStreamType { get; set; }
 
-        [Option(HelpText = "Generate Extension methods for MultipartFormData methods.", Default = true)]
+        [Option("GenerateMultipartFormDataExtensionMethods", HelpText = "Generate Extension methods for MultipartFormData methods.", Default = true)]
         public bool GenerateMultipartFormDataExtensionMethods { get; set; }
 
-        [Option(HelpText = "Generate Extension methods for ApplicationOctetStream methods.", Default = true)]
+        [Option("GenerateApplicationOctetStreamExtensionMethods", HelpText = "Generate Extension methods for ApplicationOctetStream methods.", Default = true)]
         public bool GenerateApplicationOctetStreamExtensionMethods { get; set; } = true;
 
-        [Option(HelpText = "Generate Extension methods for FormUrlEncoded methods.", Default = true)]
+        [Option("GenerateFormUrlEncodedExtensionMethods", HelpText = "Generate Extension methods for FormUrlEncoded methods.", Default = true)]
         public bool GenerateFormUrlEncodedExtensionMethods { get; set; } = true;
 
-        [Option("returnobjectfrommethod", HelpText = "Return Object from Method when Response is defined but no Model is specified.", Default = false)]
+        [Option("ReturnObjectFromMethod", HelpText = "Return Object from Method when Response is defined but no Model is specified.", Default = false)]
         public bool ReturnObjectFromMethodWhenResponseIsDefinedButNoModelIsSpecified { get; set; }
 
-        [Option(HelpText = "Preferred Content-Type to use when both 'application/json' and 'application/xml' are defined.", Default = ContentType.ApplicationJson)]
+        [Option("PreferredContentType", HelpText = "Preferred Content-Type to use when both 'application/json' and 'application/xml' are defined.", Default = ContentType.ApplicationJson)]
         public ContentType PreferredContentType { get; set; }
 
-        [Option(HelpText = "Always use Content-Type 'application/json', also when multiple Content-Types are are defined.", Default = false)]
+        [Option("ForceContentTypeToApplicationJson", HelpText = "Always use Content-Type 'application/json', also when multiple Content-Types are are defined.", Default = false)]
         public bool ForceContentTypeToApplicationJson { get; set; }
 
-        [Option(HelpText = "Use the OperationId as method name, if valid.", Default = true)]
+        [Option("UseOperationIdAsMethodName", HelpText = "Use the OperationId as method name, if valid.", Default = true)]
         public bool UseOperationIdAsMethodName { get; set; } = true;
 
-        [Option(HelpText = "Preferred SecurityDefinition type to add to the interface.", Default = SecurityDefinitionType.Automatic)]
+        [Option("PreferredSecurityDefinitionType", HelpText = "Preferred SecurityDefinition type to add to the interface.", Default = SecurityDefinitionType.Automatic)]
         public SecurityDefinitionType PreferredSecurityDefinitionType { get; set; } = SecurityDefinitionType.Automatic;
 
-        [Option(HelpText = "Append '= null' to optional parameters in the interface methods.", Default = true)]
+        [Option("MakeNonRequiredParametersOptional", HelpText = "Append '= null' to optional parameters in the interface methods.", Default = true)]
         public bool MakeNonRequiredParametersOptional { get; set; }
 
-        [Option(HelpText = "Define all method-headers on the interface.", Default = false)]
+        [Option("DefineAllMethodHeadersOnInterface", HelpText = "Define all method-headers on the interface.", Default = false)]
         public bool DefineAllMethodHeadersOnInterface { get; set; }
 
-        [Option(HelpText = "Define all shared method query parameters on the interface.", Default = true)]
+        [Option("DefineSharedMethodQueryParametersOnInterface", HelpText = "Define all shared method query parameters on the interface.", Default = true)]
         public bool DefineSharedMethodQueryParametersOnInterface { get; set; }
 
-        [Option(HelpText = "Generate and use model for 'AnyOf' and 'OneOf' return types.", Default = true)]
+        [Option("GenerateAndUseModelForAnyOfOrOneOf", HelpText = "Generate and use model for 'AnyOf' and 'OneOf' return types.", Default = true)]
         public bool GenerateAndUseModelForAnyOfOrOneOf { get; set; }
 
-        [Option(HelpText = "Preferred MultipleResponsesType to use when multiple responses are defined for a path.", Default = MultipleResponsesType.AnyOf)]
+        [Option("PreferredMultipleResponsesType", HelpText = "Preferred MultipleResponsesType to use when multiple responses are defined for a path.", Default = MultipleResponsesType.AnyOf)]
         public MultipleResponsesType PreferredMultipleResponsesType { get; set; }
         #endregion
 
         #region Models
-        [Option("generateprimitivepropertiesasnullable", HelpText = "Generate properties as nullable for OpenApi 2.0", Default = false)]
+        [Option("GeneratePrimitivePropertiesAsNullable", HelpText = "Generate properties as nullable for OpenApi 2.0", Default = false)]
         public bool GeneratePrimitivePropertiesAsNullableForOpenApi20 { get; set; }
 
-        [Option(HelpText = "Support vendor extension 'x-nullable' to indicate a property as nullable for OpenApi 2.0", Default = false)]
+        [Option("SupportExtensionXNullable", HelpText = "Support vendor extension 'x-nullable' to indicate a property as nullable for OpenApi 2.0", Default = false)]
         public bool SupportExtensionXNullable { get; set; }
         #endregion
     }
